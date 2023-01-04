ce5576da49fc4cbf39e07fcacc4d17de283fab6f
Krishna Kumar <krishnak@utexas.edu>
2023-01-03 21:30:03 -0600

    Shape fn doc

--- LearnMPM/shapefn.py
+++ LearnMPM/shapefn.py
@@ -1,9 +1,19 @@
 import numpy as np
 
 class Linear1D:
+    """Define linear 1D shape function for element.
+
+    """
     def sf(self, xi):
         """
-        Linear 1D shape function in natural coordinates
+        Linear 1D shape function in natural coordinates.
+
+        Arguments:
+            xi: float
+                location in natural coordinates
+        
+        Returns:
+            shape function of 1D linear element in natural coords.
         """
         shapefn = np.array([0.5 * (1 - xi), 0.5 * (1 + xi)])
         return shapefn
@@ -11,16 +21,45 @@ class Linear1D:
     def gradsf(self, xi):
         """
         Linear 1D gradient of shape function in natural coordinates.
+        
+        Arguments:
+            xi: float
+                location in natural coordinates
+        
+        Returns:
+            Gradient of shape function of 1D linear element.
         """
         gradsf = np.array([-0.5, 0.5])
         return gradsf
     
     def dn_dx(self, xi, coords):
+        """
+        Linear 1D gradient of shape function in physical coordinates.
+
+        Arguments:
+            xi: float
+                location in natural coordinates
+            coords: array
+                Nodal coordinates
+
+        Returns:
+            Gradient of 1D shape fn in physical coords at xi
+        """
         length = abs(coords[0] - coords[1])
         # dN/dx = dN/dxi * dxi/dx
         return self.gradsf(xi) * 2. / length
     
     def gauss_pts(self, ngauss):
+        """
+        Gauss point coordinates of an element
+
+        Arguments:
+            ngauss: int
+                Number of gauss points
+        
+        Returns:
+            Location of gauss points in the element
+        """
         if ngauss == 1:
             return np.array([0])
         elif ngauss == 2:
